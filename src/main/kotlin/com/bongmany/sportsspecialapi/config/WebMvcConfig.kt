f88056084f2a7dcc4ecea8ea621ae5452b59a182
kimbyeongmin <kbm0598@gmail.com>
2021-02-10 14:51:11 +0900

    Add cors allow configuration

--- src/main/kotlin/com/bongmany/sportsspecialapi/config/WebMvcConfig.kt
+++ src/main/kotlin/com/bongmany/sportsspecialapi/config/WebMvcConfig.kt
@@ -1,13 +1,16 @@
 package com.bongmany.sportsspecialapi.config
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Configuration
 class WebMvcConfig : WebMvcConfigurer {
 
     // Allow all Cors
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+            .allowedOrigins("*")
     }
 
 }
