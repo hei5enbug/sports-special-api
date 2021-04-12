ae969eab437221a12f1c66efaa5cf7ef00a9b4fa
kimbyeongmin <kbm0598@gmail.com>
2021-04-12 22:42:15 +0900

    delete test

--- src/test/kotlin/com/bongmany/sportsspecialapi/SportsSpecialApiApplicationTests.kt
+++ src/test/kotlin/com/bongmany/sportsspecialapi/SportsSpecialApiApplicationTests.kt
@@ -1,14 +1,10 @@
 package com.bongmany.sportsspecialapi
 
-import com.bongmany.sportsspecialapi.service.NBAService
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SportsSpecialApiApplicationTests {
-
-    @Autowired private lateinit var nbaService: NBAService
 
     @Test
     fun TestNBAService() {
